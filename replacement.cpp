c2e9100d5e32b379adf72c90cc59abe0acbbcfb5
Amanda Bienz <bienz@Amandas-Work-MacBook-Pro.local>
2021-03-01 13:06:12 -0700

    Added solutions

--- replacement.cpp
+++ replacement.cpp
@@ -5,7 +5,8 @@
 // Return how many frames you stepped through to find replacement frame
 int fifo(FrameList* active_frames, FrameList** frame_to_remove_ptr)
 {
-
+    *frame_to_remove_ptr = active_frames;
+    return 1;
 }
 
 // Delete Least Recently Used Frame
@@ -15,7 +16,29 @@ int fifo(FrameList* active_frames, FrameList** frame_to_remove_ptr)
 // Return how many frames you stepped through to find replacement frame
 int lru(FrameList* active_frames, FrameList** frame_to_remove_ptr)
 {
+    int idx;
+    int min_idx = active_frames->idx;
+    int ctr = 1;
+    int min_ctr = ctr;
+    FrameList* min_frame = active_frames;
+    FrameList* prev_frame = NULL;
 
+    while (active_frames->next != NULL)
+    {
+        ctr++;
+        idx = active_frames->next->idx;
+        if (idx < min_idx)
+        {
+            min_ctr = ctr;
+            min_idx = idx;
+            min_frame = active_frames->next;
+            prev_frame = active_frames;
+        }
+        active_frames = active_frames->next;
+    }
+
+    *frame_to_remove_ptr = min_frame;
+    return ctr;
 }
 
 // Step through active frames, starting with first element in linked list (passed as arg)
@@ -25,5 +48,32 @@ int lru(FrameList* active_frames, FrameList** frame_to_remove_ptr)
 // Return how many frames you stepped through to find replacement frame
 int clock_lru(FrameList* active_frames, FrameList** frame_to_remove_ptr)
 {
+    int ctr = 0;
+    FrameList* first_frame = active_frames;
+    FrameList* prev_frame = NULL;
 
+    while (1)
+    {
+        ctr++;
+        if (active_frames->clock_bit == 0)
+        {
+            *frame_to_remove_ptr = active_frames;
+            break;
+        }
+        else
+        {
+            active_frames->clock_bit = 0;
+            printf("IDX %d\n", active_frames->idx);
+        }
+
+        prev_frame = active_frames;
+        active_frames = active_frames->next;
+        if (active_frames == NULL)
+        {
+            prev_frame = NULL;
+            active_frames = first_frame;
+        }
+    }
+
+    return ctr;
 }
--- virtual_to_physical.cpp
+++ virtual_to_physical.cpp
@@ -7,8 +7,21 @@
 //      - addr_bits : number of bits in virtual address
 void split_virtual_address(int virtual_address, int page_size, int* VPN, int* offset)
 {
+    int offset_bits = log2(page_size);
+    int vpn_bits = sizeof(int)*BITS_PER_BYTE - offset_bits;
 
+    int mask = 0;
+    for (int i = 0; i < vpn_bits; i++)
+        mask = mask | (1 << (offset_bits + i));
+    *VPN = (virtual_address & mask) >> offset_bits;
+
+    mask = 0;
+    for (int i = 0; i < offset_bits; i++)
+        mask = mask | (1 << (i));
+    *offset = virtual_address & mask;
 }
+
+
 
 
 
@@ -16,7 +29,16 @@ void split_virtual_address(int virtual_address, int page_size, int* VPN, int* of
 // Assuming k-way associative TLB
 void split_VPN(int VPN, int k, int* index, int* tag)
 {
-
+    int idx_bits = log2(k);
+    int tag_bits = sizeof(VPN)*BITS_PER_BYTE - idx_bits;
+    int idx_mask = 0;
+    for (int i = 0; i < idx_bits; i++)
+        idx_mask = idx_mask | (1 << i);
+    int tag_mask = 0;
+    for (int i = 0; i < tag_bits; i++)
+        tag_mask = tag_mask | (1 << (idx_bits + i));
+    *index = (VPN & idx_mask);
+    *tag = (VPN & tag_mask) >> idx_bits;
 }
 
 
@@ -28,7 +50,27 @@ void split_VPN(int VPN, int k, int* index, int* tag)
 //     - Otherwise, return false
 int TLB_lookup(TLB* tlb, int VPN)
 {
-
+    int index, tag;
+    split_VPN(VPN, tlb->k, &index, &tag);
+    
+    TLB_entry* entry = NULL;
+    bool success = tlb->lookup(index, tag, &entry);
+    if (success)
+    {
+        if (entry->can_access())
+        {
+            return entry->PFN;
+        }
+        else
+        {
+            protection_fault();
+        }
+    }
+    else
+    {
+        tlb_miss();
+    }
+    return 0;
 }
 
 
@@ -38,7 +80,33 @@ int TLB_lookup(TLB* tlb, int VPN)
 // Lookup VNP in Page Table
 int table_lookup(PageTable* table, TLB* tlb, int VPN)
 {
-
+    int index, tag;
+    PTE* entry = table->lookup(VPN);
+    if (entry->valid_bit)
+    {
+        if (entry->can_access())
+        {
+            if (entry->present_bit)
+            {
+                split_VPN(VPN, tlb->k, &index, &tag);
+                tlb->add_entry(index, tag, entry->PFN, entry->protect_bit);
+                return entry->PFN;
+            }
+            else
+            {
+                page_fault();
+            }
+        }
+        else
+        {
+            protection_fault();
+        }
+    }
+    else
+    {
+        segmentation_fault();
+    }
+    return 0;
 }
 
 
@@ -48,7 +116,9 @@ int table_lookup(PageTable* table, TLB* tlb, int VPN)
 // Convert PFN and Offset to Physical Address
 int get_physical_address(int PFN, int offset, int page_size)
 {
+    int offset_bits = log2(page_size);
 
+    return ((PFN << offset_bits) | offset);
 }
 
 
@@ -58,6 +128,21 @@ int get_physical_address(int PFN, int offset, int page_size)
 // Put everything together to get physical address from virtual
 int virtual_to_physical(int virtual_address, int page_size, TLB* tlb, PageTable* table)
 {
+    int vpn, offset, pfn;
 
+    split_virtual_address(virtual_address, page_size, &vpn, &offset);
+
+    try
+    {
+        pfn = TLB_lookup(tlb, vpn);
+    }
+    catch (const char* msg)
+    {
+        if (std::string(msg) == "TLB Miss")
+            pfn = table_lookup(table, tlb, vpn);
+        else throw (msg);
+    }
+
+    return get_physical_address(pfn, offset, page_size);
 }
 
